f46a69ad73431348a7acd5478d35ce93d96963dd
Vasu Aggarwal <svasu.sv95@gmail.com>
2024-06-29 13:59:07 +0530

    Added login view model

---
+++ app/src/main/java/com/android/splitease/screens/LoginScreen.kt
@@ -0,0 +1,85 @@
+package com.android.splitease.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.android.splitease.models.responses.UserLoginResponse
+import com.android.splitease.utils.NetworkResult
+import com.android.splitease.viewmodels.LoginViewModel
+
+@Composable
+fun LoginScreen(viewModel: LoginViewModel = hiltViewModel(), navController: NavController){
+
+    val user: State<NetworkResult<UserLoginResponse>> = viewModel.user.collectAsState()
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = { viewModel.login(email, password) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Login")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        when (val result = user.value) {
+            is NetworkResult.Idle -> {
+
+            }
+
+            is NetworkResult.Loading -> {
+                CircularProgressIndicator()
+            }
+            is NetworkResult.Success -> {
+                navController.navigate("bottomBar")
+                Text(text = "Logged in successfully !!")
+            }
+
+            is NetworkResult.Error -> {
+                Text(text = result.message.toString())
+            }
+        }
+    }
+}
