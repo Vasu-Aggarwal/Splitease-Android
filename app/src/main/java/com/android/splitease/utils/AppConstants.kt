76b3588061eeb32a5bd2d4a63d99693e9cb9dfc6
vasu.aggarwal <vasu.aggarwal@extramarks.com>
2024-07-26 11:13:17 +0530

    Added colors in the project

--- app/src/main/java/com/android/splitease/utils/AppConstants.kt
+++ app/src/main/java/com/android/splitease/utils/AppConstants.kt
@@ -3,7 +3,7 @@ package com.android.splitease.utils
 import java.util.concurrent.TimeUnit
 
 object AppConstants {
-    const val OFFICE_BASE_URL = "http://10.100.100.144:9090"
+    const val OFFICE_BASE_URL = "http://10.100.100.210:9090"
     const val HOME_BASE_URL = "http://192.168.1.24:9090"
     const val TRANSACTION_URL = "/api/transaction"
     const val AUTH_URL = "/auth"
