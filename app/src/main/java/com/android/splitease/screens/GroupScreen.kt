d71283c0113f3e879568420d859276366c674455
vasu.aggarwal <vasu.aggarwal@extramarks.com>
2024-07-26 17:39:49 +0530

    cosmetic changes

--- app/src/main/java/com/android/splitease/screens/GroupScreen.kt
+++ app/src/main/java/com/android/splitease/screens/GroupScreen.kt
@@ -1,6 +1,7 @@
 package com.android.splitease.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,13 +13,16 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -82,30 +86,44 @@ fun GroupScreen(viewModel: GroupViewModel = hiltViewModel(), navController: NavC
 
 @Composable
 fun GroupItem(group: AddGroupResponse, viewModel: GroupViewModel, navController: NavController) {
-    Row(modifier = Modifier.padding(8.dp)) {
-        group.imageUrl?.let { url ->
-            Image(
-                painter = rememberAsyncImagePainter(
-                    ImageRequest.Builder(LocalContext.current).data(data = url).apply(block = fun ImageRequest.Builder.() {
-                        crossfade(true)
-                    }).build()
-                ),
-                contentDescription = "Image",
-                modifier = Modifier
-                    .size(100.dp) // Adjust size as needed
-                    .padding(end = 8.dp)
-            )
-        }
-        Column {
-            Text(text = group.name, modifier = Modifier.padding(bottom = 4.dp))
-            // You can add more details if needed
+    Card(
+        colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+        onClick = {
+            navController.navigate(Screen.DetailedGroupScreen.createRoute(group.groupId))
+        },
+        modifier = Modifier
+            .padding(4.dp)
+            .height(100.dp)
+            .fillMaxWidth()
+    ) {
+        Row(modifier = Modifier.padding(8.dp)) {
+            group.imageUrl?.let { url ->
+                Image(
+                    painter = rememberAsyncImagePainter(
+                        ImageRequest.Builder(LocalContext.current).data(data = url)
+                            .apply(block = fun ImageRequest.Builder.() {
+                                crossfade(true)
+                            }).build()
+                    ),
+                    contentDescription = "Image",
+                    modifier = Modifier
+                        .size(100.dp) // Adjust size as needed
+                        .padding(end = 8.dp)
+                )
+            }
+            Column {
+                Text(text = group.name, modifier = Modifier.padding(bottom = 4.dp))
+                // You can add more details if needed
+            }
         }
     }
 }
 
 @Composable
 fun StartNewGroup(navController: NavController) {
-    Button(onClick = { navController.navigate(Screen.NewGroupScreen.route) }) {
+    Button(
+        onClick = { navController.navigate(Screen.NewGroupScreen.route) },
+    ) {
         Text(text = "Start a new group")
     }
 }
